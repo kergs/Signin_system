f71591c29043a65d9479914ca5628254baffd5db
kergs <cchisomfrancis@gmail.com>
2022-10-05 01:01:08 +0100

    html

--- main/forms.py
+++ main/forms.py
@@ -8,8 +8,8 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password", "confirm password",
-         "Phone Number")
+        fields = ("username", "email", "password1", "password2"
+         )
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
